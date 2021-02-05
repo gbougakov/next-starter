d4c0c4af712801e5b7fc8abce64c81e7a7ee5cab
George Bougakov <g@bygeorgenet.me>
2021-02-05 21:09:26 +0300

    Remove comments

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,5 @@ import '../styles/index.css';
 init()
 
 export default function App({ Component, pageProps }) {
-  // Workaround for https://github.com/vercel/next.js/issues/8592
   return <Component {...pageProps} />
 }
